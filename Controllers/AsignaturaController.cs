00b7347e29a7bfab4ca14a1f4799cb511746ba57
Anhy <anahi.z.a@gmail.com>
2019-09-03 19:02:11 -0500

    Ajustes en Enrutamiento

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -10,18 +10,28 @@ namespace ASP.Controllers
 {
     public class AsignaturaController : Controller
     {
+        [Route("Asignatura/Index")]
+        [Route("Asignatura/Index/{id}")]
+
+         public IActionResult Index(string id)
+        {        
+            if(!string.IsNullOrWhiteSpace(id))  
+            {
+                var asignatura = from asig in _context.Asignaturas
+                                where asig.Id == id
+                                select asig;
+                return View(asignatura.FirstOrDefault());
+
+            }     
+            else{
+                return View("MultiAsignatura",_context.Asignaturas);
+            }
+        }
         public IActionResult MultiAsignatura()        {
              
            
             ViewBag.Fecha=DateTime.Now;
             return View("MultiAsignatura",_context.Asignaturas);
-        }
-
-         public IActionResult Index()
-        {
-                 
-           
-            return View(_context.Asignaturas.FirstOrDefault());
         }
 
         private EscuelaContext _context;
